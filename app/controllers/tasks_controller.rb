482049dedcbf8ea523d7222438acd4bc98f55b60
Chris Haegner <chris@stoyo.io>
2019-07-02 17:34:03 +0200

    some refactoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,27 +1,31 @@
 class TasksController < ApplicationController
+  before_action :set_restaurant, only: %i[show edit update destroy]
+
   def index
     @tasks = Task.all
   end
+
   def show
-    @task = Task.find(params[:id])
   end
+
   def new
     @task = Task.new
   end
+
   def create
     Task.create(restaurant_params)
     redirect_to tasks_path
   end
+
   def edit
-    @task = Task.find(params[:id])
   end
+
   def update
-    @task = Task.find(params[:id])
     @task.update(restaurant_params)
     redirect_to tasks_path
   end
+
   def destroy
-    @task = Task.find(params[:id])
     @task.delete
     redirect_to tasks_path
   end
@@ -30,5 +34,9 @@ class TasksController < ApplicationController
 
   def restaurant_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_restaurant
+    @task = Task.find(params[:id])
   end
 end
